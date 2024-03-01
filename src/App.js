e2f2d93dd53d6fe0eeda00d8155a92cb810bb29b
Gayatri <gayatripande099@gmail.com>
2024-03-01 11:14:20 +0530

    made components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Project from './components/Projects';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <>
-    <div className='App'>
-      <header className='App-header'>
-    <p>Hi!!,Welcome to Gayatri's Portfolio</p>
-    </header>
-    </div>
-    </>
+    <div>
+      <Home/>
+      <About/>
+      <Project/>
+      <Blog/>
+      <Contact/>
+    </div>
+    
+  
+   </>
   );
 }
 
